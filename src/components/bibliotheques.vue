314e1c0d5ac1a731562f05772e5c2fd5b74e435f
shadri25 <sarah.hadri@edu.univ-fcomte.fr>
2022-01-06 00:21:20 +0100

    Ajout page bibliotheques
    Implementation axios pour gestion flux json

---
+++ src/components/bibliotheques.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="bibliotheques">
+  <div v-for="bibliotheque in listeBibliotheques" :key="bibliotheque.id" class="bibliotheque">
+    <h2>{{ bibliotheque.acf.titre }}</h2>
+    <p>{{ bibliotheque.acf.description }}</p>
+    <img :src="bibliotheque.acf.image.url" alt="Image">
+    <p>{{ bibliotheque.acf.adresse }}</p>
+    <a v-bind:href="bibliotheque.acf.site_web">Site web</a>
+  </div>
+  </div>
+</template>
+
+<script>
+import param from '@/param/param'
+import axios from 'axios';
+import _ from 'lodash';
+export default {
+  name: 'bibliotheques',
+  data() {
+    return {
+      listeBibliotheques: []
+    }
+  },
+  created() {
+    // Liste des sections
+    axios.get(param.host + 'bibliotheques')
+      .then(response => {
+        console.log('Réponse :', response);
+        this.listeBibliotheques = response.data;
+      })
+      .catch(error => console.log(error))
+  },
+
+}</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('../assets/graphic-styles.less');
+@import url('../assets/layout.less');
+
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
 import inscription from '@/components/inscription'
-/*import formations from '@/components/formations'*/
+import bibliotheques from '@/components/bibliotheques'
 
 
 Vue.use(Router)
@@ -10,7 +10,8 @@ Vue.use(Router)
 export default new Router({
   routes: [
     { path: '/',                 name: 'home',                       component: home },
-    { path: '/inscription',      name: 'inscription',                component: inscription }
+    { path: '/inscription',      name: 'inscription',                component: inscription },
+    { path: '/bibliotheques',    name: 'bibliotheques',              component: bibliotheques }
     //{ path: '/formations',      name: 'formations',                component: formation }
 
 
